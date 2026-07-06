24ef11bf2249d4da50847b64d519ed67c9e0b3bc
Mahir <mahirmalde2004@gmail.com>
2026-07-07 03:48:15 +0530

    Initial Commit

---
+++ InnerCircle/InnerCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnerCircleApp.swift
+//  InnerCircle
+//
+//  Created by Mahir Malde on 07/07/26.
+//
+
+import SwiftUI
+
+@main
+struct InnerCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
